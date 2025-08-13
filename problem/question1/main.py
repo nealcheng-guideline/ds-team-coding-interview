b736367b6002796b3566a4358cb109a1fa8427a0
y.kim <y.kim@guideline.com>
2025-08-13 11:32:09 -0700

    update question1

--- problem/question1/main.py
+++ problem/question1/main.py
@@ -2,11 +2,9 @@
 Prompt: You have joined a new team and are working on migrating a legacy codebase to a new platform.
 You found the following function. Take a few minutes to read the code.
 
-
 The Decimal class in Python's standard library offers exact decimal arithmetic, which makes it a perfect fit 
-for domains like finance, accounting or anywhere you can't afford those pesky binary'floating rounding errors.
+for domains like finance, accounting or anywhere you can't afford those pesky binary floating rounding errors.
 It can be used like a number Decimal(1.22) + Decimal(0.79)
-
 """
 
 
@@ -18,12 +16,12 @@ def to_decimal(in_dollars):
         in_dollars = float(in_dollars)
         in_dollars = Decimal(in_dollars)
         return in_dollars
-    except ValueError:
-        if not in_dollars:
+    except ValueError:  # cases when input string cannot be coerced into a float
+        if not in_dollars:  # cases when input string is an empty string
             return 0
         raise ValueError(f"Unable to parse field with value {in_dollars} as float")
     except TypeError:
-        if in_dollars is None:
+        if in_dollars is None:  # cases when input string is None
             logger = logging.getLogger(__name__)
             logger.debug("Trying to convert None to float - defaulting to 0")
             return 0
@@ -31,11 +29,11 @@ def to_decimal(in_dollars):
 
 
 """
-Q1: What does this function do? Describe in 1-2 sentences.
+Q1.1: What does this function do? Describe in 1-2 sentences.
 """
 
 """
-Q2: What will be the outputs of the following? Based on the code, infer the value and type of the output.
+Q1.2: What will be the outputs of the following? Based on the code, infer the value and type of the output.
 
 to_decimal("100")
 to_decimal("")
@@ -45,23 +43,21 @@ to_decimal(0.0)
 """
 
 """
-Q3: Given that this is a production code, what are the potential issues with the code? 
-Consider type safety, error handling, and maintainability.
+Q1.3: Given that this is a production code, what are the potential issues with the code? 
 """
 
 """
-Q4: Rewrite the function to be more robust given the issues that you've identified & explain your changes.
-Let's run the script to see if the function is robust.
+Q1.4: Rewrite the function to be more robust given the issues that you've identified & explain your changes.
+You can modify and run the main function below to test the provided examples and verify that your function behaves as expected.
 """
 
 """
-Q5: You are then told to add a functionality to convert dollars to cents in the same function. 
-Discuss both pros and cons of this proposal and outline a possible implementation.
-(Candidate only need to discuss whether this is a good solution and how to implement it)
+[Extra Credit] Q1.5: You are then told to add a functionality to convert dollars to cents in the same function. 
+Do you think this is a good idea?
 """
 
 if __name__ == "__main__":
-    for _input in (None, "0", 0, 0.0):
+    for _input in (None, "", "0", 0, 0.0):
         answer = to_decimal(_input)
         if answer != Decimal(0):
             raise AssertionError(f"Error: {_input} should be converted to {Decimal(0)}")
